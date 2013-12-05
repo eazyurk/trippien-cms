f6426aa8e25b9e1c55fd3337c53eddb679226a0b
eazyurk <janwillem.urk@gmail.com>
2013-12-05 19:39:06 +0100

    CMS
    Login

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title></title>
+</head>
+<body>
+<div class="container">
+<div style="height: 30%;"></div>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 login">
+    		<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<h3 class="panel-title" style="text-align: center;">Trippien CMS Login</h3>
+			 	</div>
+			  	<div class="panel-body">
+			    	<form accept-charset="UTF-8" role="form" method="POST" action="login.php">
+                    <fieldset>
+        	    	  	<div class="form-group">
+			    		    <input class="form-control" placeholder="Gebruikersnaam" name="username" type="text">
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="form-control" placeholder="Wachtwoord" name="password" type="password" value="">
+			    		</div>
+			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+			    	</fieldset>
+			      	</form>
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+try {
+	$dbh = new PDO("mysql:host=localhost;dbname=cms", "root", "");
+	} catch(PDOException $e) {
+	echo $e->getMessage();
+}
+
+$query = "SELECT * FROM `users` WHERE LOWER(`username`) = :username";
+$stmt = $dbh->prepare($query);
+$stmt->bindValue(':username', strtolower($_POST['username']));
+$stmt->execute();
+
+if($stmt->rowCount() == 1) {
+	$row = $stmt->fetch(PDO::FETCH_ASSOC);
+	require('blowfish.class.php');
+	$bcrypt = new Bcrypt(4);
+	if($bcrypt->verify($_POST['password'], $row['password'])){
+		echo "Logged in";
+	}
+}
